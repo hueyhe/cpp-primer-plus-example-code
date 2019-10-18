819c71ddfefae7fe4bfbdf42347b56ded0ff5be4
Huey <sihonghuey@163.com>
2019-10-18 20:18:28 +0800

    Add code of section 8, chapter 4

---
+++ chapter4/section8/addptrs.cpp
@@ -0,0 +1,35 @@
+// pointer addition
+
+#include <iostream>
+
+int main() {
+  using namespace std;
+
+  double wages[3] = {10000.0, 20000.0, 30000.0};
+  short stacks[3] = {3, 2, 1};
+
+  double* pw = wages;
+  short* ps = &stacks[0];
+
+  cout << "pw = " << pw << ", *pw = " << *pw << endl;
+  pw += 1;
+  cout << "add 1 to the pw pointer!\n";
+  cout << "pw = " << pw << ", *pw = " << *pw << "\n\n";
+
+  cout << "ps = " << ps << ", *ps = " << *ps << endl;
+  ps += 1;
+  cout << "add 1 to the ps pointer!\n";
+  cout << "ps = " << ps << ", *ps = " << *ps << "\n\n";
+
+  cout << "access two elements with array notation\n";
+  cout << "stacks[0] = " << stacks[0]
+       << ", stacks[1] = " << stacks[1] << endl;
+  cout << "access two elements with pointer notation\n";
+  cout << "*stacks = " << *stacks
+       << ", *(stacks + 1) = " << *(stacks + 1) << endl;
+
+  cout << sizeof(wages) << " = size of wages array\n";
+  cout << sizeof(pw) << " = size of pw pointer\n";
+
+  return 0;
+}

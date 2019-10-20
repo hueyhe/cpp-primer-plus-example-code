c541942f44879c53a26c3219d4822438c639c912
Huey <sihonghuey@163.com>
2019-10-20 09:28:23 +0800

    Add code of section 2, chapter 5

---
+++ chapter5/section2/while.cpp
@@ -0,0 +1,21 @@
+// introducing the while loop
+
+#include <iostream>
+
+const int ArSize = 20;
+
+int main() {
+  using namespace std;
+
+  char name[ArSize];
+  cout << "Your first name, please: ";
+  cin >> name;
+  cout << "Here is your name, verticalized and ASCIIized:\n";
+  int i = 0;
+  while (name[i] != '\0') {
+    cout << name[i] << ": " << int(name[i]) << endl;
+    i += 1;
+  }
+
+  return 0;
+}

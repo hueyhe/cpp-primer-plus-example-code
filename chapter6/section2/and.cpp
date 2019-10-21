8dc4d60f2b51775f2dd4d685bc2f935bddc803f2
Huey <sihonghuey@163.com>
2019-10-21 16:35:12 +0800

    Add code of section 2, chapter 6

---
+++ chapter6/section2/and.cpp
@@ -0,0 +1,46 @@
+// using the logical AND operator
+
+#include <iostream>
+
+const int ArSize = 6;
+
+int main() {
+  using namespace std;
+
+  float naaq[ArSize];
+  cout << "Enter the NAAQs (New Age Awareness Quotients) "
+       << "of\nyour neighbors. Program terminates "
+       << "when you make\n" << ArSize << " entries "
+       << "or enter a negative value.\n";
+
+  int i = 0;
+  float temp;
+  cout << "First value: ";
+  cin >> temp;
+  while (i < ArSize && temp >= 0) {
+    naaq[i] = temp;
+    i += 1;
+    if (i < ArSize) {
+      cout << "Next value: ";
+      cin >> temp;
+    }
+  }
+  if (i == 0) {
+    cout << "No data--bye\n";
+  } else {
+    cout << "Enter your NAAQ: ";
+    float you;
+    cin >> you;
+    int count = 0;
+    for (int j = 0; j < i; j += 1) {
+      if (naaq[j] > you) {
+        count += 1;
+      }
+    }
+    cout << count;
+    cout << " of your neighbors have greater awareness of\n"
+         << "the New Age than you do.\n";
+  }
+
+  return 0;
+}
---
+++ chapter6/section2/not.cpp
@@ -0,0 +1,30 @@
+// using the not operator
+
+#include <iostream>
+#include <climits>
+
+bool isInt(double);
+
+int main() {
+  using namespace std;
+
+  double num;
+
+  cout << "Yo, dude! Enter an integer value: ";
+  cin >> num;
+  while (!isInt(num)) {
+    cout << "Out of range -- please try again: ";
+    cin >> num;
+  }
+  int val = int(num);
+  cout << "You've entered the integer " << val << "\nBye\n";
+
+  return 0;
+}
+
+bool isInt(double x) {
+  if (x <= INT_MAX && x >= INT_MIN) {
+    return true;
+  }
+  return false;
+}

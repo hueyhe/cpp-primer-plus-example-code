b403a22e46eb7301328b0eb21ace513ea119a5d5
Huey <sihonghuey@163.com>
2019-10-28 15:04:25 +0800

    Add code of section 2, chapter 7

---
+++ chapter7/section2/twoarg.cpp
@@ -0,0 +1,33 @@
+// a function with 2 arguments
+
+#include <iostream>
+
+using namespace std;
+
+void n_chars(char, int);
+
+int main() {
+  int times;
+  char ch;
+
+  cout << "Enter a character: ";
+  cin >> ch;
+  while (ch != 'q') {
+    cout << "Enter a integer: ";
+    cin >> times;
+    n_chars(ch, times);
+    cout << "\nEnter another character or press the"
+            " q-key to quit: ";
+    cin >> ch;
+  }
+  cout << "The value of times is " << times << ".\n";
+  cout << "Bye\n";
+
+  return 0;
+}
+
+void n_chars(char c, int n) {
+  while (n-- > 0) {
+    cout << c;
+  }
+}

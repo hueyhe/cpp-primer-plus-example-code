b0a028978efb5bb6cfe8ed8216433ae8f9b81b52
Huey <sihonghuey@163.com>
2019-10-31 15:57:04 +0800

    Add code of section 7, chapter 7

---
+++ chapter7/section7/topfive.cpp
@@ -0,0 +1,30 @@
+// handling an array of string objects
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+const int SIZE = 5;
+
+void display(const string sa[], int n);
+
+int main() {
+  string list[SIZE];
+  cout << "Enter your " << SIZE << " favorite astronomical sights:\n";
+  for (int i = 0; i < SIZE; i += 1) {
+    cout << i + 1 << ": ";
+    getline(cin, list[i]);
+  }
+
+  cout << "Your list:\n";
+  display(list, SIZE);
+
+  return 0;
+}
+
+void display(const string sa[], int n) {
+  for (int i = 0; i < n; i += 1) {
+    cout << i + 1 << ": " << sa[i] << endl;
+  }
+}

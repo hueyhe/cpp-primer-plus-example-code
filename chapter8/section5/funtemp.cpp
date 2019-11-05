e41359c4790fd61cf7a4bd24f5d7d01a118b0875
Huey <sihonghuey@163.com>
2019-11-05 19:33:08 +0800

    Add code of section 5, chapter 8

---
+++ chapter8/section5/funtemp.cpp
@@ -0,0 +1,34 @@
+// using a function template
+
+#include <iostream>
+
+template <typename T>
+void Swap(T& a, T& b);
+
+int main() {
+  using namespace std;
+
+  int i = 10;
+  int j = 20;
+  cout << "i, j = " << i << ", " << j << ".\n";
+  cout << "using compiler-generated int swapper:\n";
+  Swap(i, j);
+  cout << "Now i, j = " << i << ", " << j << ".\n";
+
+  double x = 24.5;
+  double y = 81.7;
+  cout << "x, y = " << x << ", " << y << ".\n";
+  cout << "using compiler-generated double swapper:\n";
+  Swap(x, y);
+  cout << "Now x, y = " << x << ", " << y << ".\n";
+
+  return 0;
+}
+
+template <typename T>
+void Swap(T& a, T& b) {
+  T temp;
+  temp = a;
+  a = b;
+  b = temp;
+}

a558ea45c09fd4bb4bcb129304248bfe588c0bd8
Huey <sihonghuey@163.com>
2019-11-05 15:29:26 +0800

    Add code of section 3, chapter 8

---
+++ chapter8/section3/left.cpp
@@ -0,0 +1,38 @@
+// string function with a default argument
+
+#include <iostream>
+
+const int ArSize = 80;
+
+char* left(const char* str, int n = 1);
+
+int main() {
+  using namespace std;
+
+  char sample[ArSize];
+  cout << "Enter a string:\n";
+  cin.get(sample, ArSize);
+  char* ps = left(sample, 4);
+  cout << ps << endl;
+  delete [] ps;
+  ps = left(sample);
+  cout << ps << endl;
+  delete [] ps;
+
+  return 0;
+}
+
+char* left(const char* str, int n) {
+  if (n < 0) {
+    n = 0;
+  }
+  char* p = new char[n + 1];
+  int i;
+  for (i = 0; i < n && str[i]; i += 1) {
+    p[i] = str[i];
+  }
+  while (i <= n) {
+    p[i++] = '\0';
+  }
+  return p;
+}
